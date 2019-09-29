848894fecb0b7862475ff1067b3d2cf2d63243b9
Ruben Taelman <rubensworks@gmail.com>
2019-09-29 12:30:05 +0200

    Add missing mock for tests

---
+++ test/__mocks__/follow-redirects.js
@@ -0,0 +1,40 @@
+const IncomingMessage = require('http').IncomingMessage;
+
+let options = {
+  statusCode: 200
+};
+
+function mockSetup(mock) {
+  options = mock;
+}
+
+function request(settings, func) {
+  let body = new IncomingMessage();
+  Object.assign(body, options.body || {}, {
+    input: settings,
+    setEncoding: () => {},
+    headers: options.headers || {},
+    statusCode: options.statusCode,
+    url: settings.url,
+    responseUrl: settings.url,
+  });
+  setImmediate(() => func(body));
+
+  return {
+    abort: () => { },
+    on: (type, callback) => {
+      if (type === 'error' && options.error) {
+        setImmediate(() => callback(new Error('Request Error!')));
+      }
+    },
+    end: () => {}
+  }
+}
+
+function Agent() {}
+
+module.exports = {
+  http: { request, Agent },
+  https: { request, Agent },
+  mockSetup
+};
