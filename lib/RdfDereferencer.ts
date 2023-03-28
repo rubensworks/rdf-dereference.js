34ae28268f480a80fd460775beead816c1a21d7b
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-03-29 09:33:09 +1100

    fix: require 'process/' rather than 'process'

--- lib/RdfDereferencer.ts
+++ lib/RdfDereferencer.ts
@@ -1,8 +1,8 @@
 import {IActorDereferenceRdfOutput} from "@comunica/bus-dereference-rdf";
 import {join} from "path";
-import process = require("process");
 import * as RDF from "@rdfjs/types";
 import {IDereferenceOptions, RdfDereferencerBase} from "./RdfDereferencerBase";
+const process = require("process/");
 
 /**
  * An RdfDerefencer can dereference URLs to RDF streams, using any RDF serialization.
